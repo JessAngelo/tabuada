788ec55cbda7ae1db62745ca102cb3a15102f5b2
Jessica Angelo <jessica.angelo@solucx.com.br>
2024-10-23 20:11:58 -0300

    teste

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
 
-function gerarTabuada(numero, sequencia) {
-  let resultado = '';
-  for (let i = 0; i <= sequencia; i++) {
-    resultado += `${numero} x ${i} = ${numero * i}<br>`;
-  }
-  return resultado;
+function gerarTabuada(tabuada, sequencia) {
+    let resultado = `<h1>Tabuada do ${tabuada}</h1><ul>`;
+    for (let i = 0; i <= sequencia; i++) {
+        resultado += `<li>${tabuada} x ${i} = ${tabuada * i}</li>`;
+    }
+    resultado += '</ul>';
+    return resultado;
 }
 
 
-app.get('/tabuada', (req, res) => {
+function validarEntrada(tabuada, sequencia) {
+    if (isNaN(tabuada) || isNaN(sequencia) || tabuada < 0 || sequencia < 0) {
+        return false;
+    }
+    return true;
+}
 
-  const tabuada = parseInt(req.query.tabuada);
-  const sequencia = parseInt(req.query.sequencia) || 10;
 
+app.get('/', (req, res) => {
   
-  if (!isNaN(tabuada)) {
-    const resultado = gerarTabuada(tabuada, sequencia);
-    res.send(`
-      <html>
-        <head><title>Tabuada do ${tabuada}</title></head>
-        <body>
-          <h1>Feito por: Jéssica Maria de Angelo </h1>
-          <br>
-          <br>
-          <h1>Tabuada do ${tabuada}</h1>
-          <p>${resultado}</p>
-        </body>
-      </html>
-    `);
-  } else {
-    res.send('Erro: Informe um número válido para a tabuada na URL. Exemplo: ?tabuada=3&sequencia=25');
-  }
+    let tabuada = parseInt(req.query.tabuada) || 1;  
+    let sequencia = parseInt(req.query.sequencia) || 10;  
+
+
+    if (!validarEntrada(tabuada, sequencia)) {
+        return res.status(400).send('<h1>Erro: parâmetros inválidos. Insira números válidos para tabuada e sequencia.</h1>');
+    }
+
+
+    let htmlContent = gerarTabuada(tabuada, sequencia);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.send(htmlContent);
 });
 
 
-app.listen
+module.exports = app;
